33fe7da4d15465e625ef8136e290c6fe5bf0f50f
Chris Griffing <cmgriffing@gmail.com>
2017-03-09 19:49:41 -0800

    Add whois package instead of using cli

--- domains/domains.go
+++ domains/domains.go
@@ -3,8 +3,9 @@ package domains
 import (
 	"fmt"
 	"log"
-	"os/exec"
 	"regexp"
+
+    "github.com/domainr/whois"
 )
 
 func outputStatus(domain string, status string) string {
@@ -13,17 +14,20 @@ func outputStatus(domain string, status string) string {
 
 func WhoisLookup(domain string) string {
 
-	cmd := exec.Command("whois", domain)
-	result, err := cmd.Output()
+	//cmd := exec.Command("whois", domain)
+	//result, err := cmd.Output()
+
+    result, err := whois.NewRequest(domain)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	availableResult, availableError := regexp.Match("No match for domain", result)
+	availableResult, availableError := regexp.Match("No match for domain", result.Body)
 	if availableError != nil {
 		return outputStatus(domain, "error")
 	}
-	unavailableResult, unavailableError := regexp.Match("Domain Name:", result)
+	unavailableResult, unavailableError := regexp.Match("Domain Name:", result.Body)
 	if unavailableError != nil {
 		return outputStatus(domain, "error")
 	}
